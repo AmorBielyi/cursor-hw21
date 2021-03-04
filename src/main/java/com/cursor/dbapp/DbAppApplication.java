6adb6b8d02ae0d7f2ba336b231c31e2fc63b7592
Roman Bielyi <romanbielyi.amor@gmail.com>
2021-03-04 14:36:13 +0200

    init commit

---
+++ src/main/java/com/cursor/dbapp/DbAppApplication.java
@@ -0,0 +1,12 @@
+package com.cursor.dbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cursor/dbapp/DbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursor.dbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
